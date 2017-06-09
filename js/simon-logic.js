d44bdb9a0f9e8f49f1dff0112d9062c1709cb485
nickhartjenstein <hartjenstein@Hartjensteins.local>
2017-06-10 01:05:03 +0200

    added pad click logic

--- js/simon-logic.js
+++ js/simon-logic.js
@@ -3,7 +3,7 @@
 // Constants
     const MAX = 4;
     const MIN = 1;
-// ---------EventListener ---------
+// ---------Event Listeners ---------
 
 // --- On-Off Switch 
 let onOff = document.querySelector(".onoffswitch-checkbox");
@@ -14,7 +14,12 @@ let startBtn = document.querySelector(".start");
 console.log(startBtn)
 startBtn.addEventListener('click', startGame);
 
+// ----- Pad Press --------
+let pads = document.querySelectorAll(".pad");
 
+for (let pad of pads) {
+    pad.addEventListener("click", padClicked );
+}
 if(onOff.checked) {
     turnOn();
 }
@@ -40,12 +45,28 @@ function chooseColor(){
     let randomNumber =  Math.floor(Math.random() * (MAX - MIN +1)) + MIN; 
     var shapeClass = ".shape"+randomNumber;
     var iluminated = "lit-up"+randomNumber;
-
+    console.log(iluminated);
     setTimeout(function(){
-         document.querySelector(shapeClass).classList.add(iluminated);
+         lightUp(shapeClass, iluminated);
     }, 1000);
    
+   
+}
+function padClicked(e) {
+    let classes = e.target.className
+    console.log(classes)
+    classes = classes.split(" ");
+    console.log(classes)
+    let classShape = "."+classes[1];
+    console.log(classShape)
+    let classNr = classShape.substring(6, 7);
+    let ilum = "lit-up" + classNr;
+    console.log(ilum)
+    lightUp(classShape, ilum);
+}
+function lightUp(shapeClass, iluminated){
+    document.querySelector(shapeClass).classList.add(iluminated);
     setTimeout(function(){
         document.querySelector(shapeClass).classList.remove(iluminated);
-    }, 2000);
+    }, 1000);
 }
